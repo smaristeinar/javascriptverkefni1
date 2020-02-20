6e61cd13ee10f3b998d72e24103f2cd23296a418
smari steinar <smarikul@hotmail.com>
2020-02-20 18:56:57 +0000

    hellebell

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const width = canvas.width = window.innerWidth;
 const height = canvas.height = window.innerHeight;
 
 class Shape {
-  constructor(x,  y, velX, velY, color, size, id, hp, pow, shapetype/*dót til að búa til bolltar*/) {
+  constructor(x,  y, velX, velY, color, size, id, hp, pow, shapetype, destY, destX/*dót til að búa til bolltar*/) {
     this.x = x;
     this.y = y;
     this.velX = velX;
@@ -24,9 +24,16 @@ class Shape {
     this.id = id;
     this.hp = hp;
     this.pow = pow;
+    this.destY = destY
+    this.destX = destX
   }
   attack(enemy){
-    enemy -= this.pow
+    enemy -= this.pow;
+  }
+ deli(){
+  if (this.shapetype == "bolti"){
+    return true;
+  }
   }
 }
 
@@ -37,7 +44,7 @@ class Bolti extends Shape{
     ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI)
     ctx.fill();
 }
-update(/*breytingar og hegðun á boltunum*/){
+  update(/*breytingar og hegðun á boltunum*/){
   if ((this.x + this.size) >= width) {
     this.velX = -(this.velX);
   }
@@ -57,22 +64,22 @@ update(/*breytingar og hegðun á boltunum*/){
   this.x += this.velX;
     this.y += this.velY;
     }
-    coll(listofshapes){
+    coll(listiafboltum){
       /* collision aðferð */
-    for (let i = 0; i < listofshapes.length; i++){
-      if (listofshapes[i].id != this.id)
+    for (let i = 0; i < listiafboltum.length; i++){
+      if (listiafboltum[i].id != this.id)
       /*rennur í gegnum allar kúlur en sleppir sjálfum sér */
       {
-        let _size =  this.size + listofshapes[i].size; /*fær út samanlagðan radíus*/
-        let dist  = Math.hypot(this.x - listofshapes[i].x, this.y - listofshapes[i].y);
+        let _size =  this.size + listiafboltum[i].size; /*fær út samanlagðan radíus*/
+        let dist  = Math.hypot(this.x - listiafboltum[i].x, this.y - listiafboltum[i].y);
         /*notar pýþagóras til að finna lengdina á milli kúlu */
         if (dist < _size ) {
-          this.hp -= listofshapes[i].pow;
+          this.hp -= listiafboltum[i].pow;
           /*ef kúlunar snertast þá ráðast þær á hvort aðra */
-          if (listofshapes[i].hp < 0) {
-            console.log(listofshapes[i].id, "is Dead");
+          if (listiafboltum[i].hp < 0) {
+            console.log(listiafboltum[i].id, "is Dead");
             /*og svo ef hún deyr þá hverfur hún úr listanum*/
-            listofshapes.splice(i,1);
+            listiafboltum.splice(i,1);
           }
         }
 
@@ -80,7 +87,6 @@ update(/*breytingar og hegðun á boltunum*/){
       }
     }
   }
-  readytogo()
 }
 
 class Kassi extends Shape {
@@ -90,52 +96,27 @@ class Kassi extends Shape {
     ctx.rect(this.x,this.y,this.size,this.size );
     ctx.fill();
   }
-  update(/*breytingar og hegðun á boltunum*/){
-  if ((this.x + this.size) >= width) {
-    this.velX = -(this.velX);
-  }
-
-  if (this.x   <= 0) {
-    this.velX = -(this.velX);
-  }
-
-  if ((this.y + this.size) >= height) {
-    this.velY = -(this.velY);
-  }
-
-  if ((this.y) <= 0) {
-    this.velY = -(this.velY);
-  }
-
-  this.x += this.velX;
-    this.y += this.velY;
-    }
-    coll(listofshapes){
-      /* collision aðferð */
-    for (let i = 0; i < listofshapes.length; i++){
-      if (listofshapes[i].id != this.id)
-      /*rennur í gegnum allar kúlur en sleppir sjálfum sér */
-      {
-          if(listofshapes[i].shapetype == "kassi"){
-            let _size =  (this.size / 2) + (listofshapes[i].size / 2); /*fær út samanlagðan radíus*/
-            let dist  = Math.hypot((this.x * 2) - (listofshapes[i].x * 2) , (this.y *  2) - (listofshapes[i].y * 2)); 
-            if (dist < _size) {
-              console.log("boop");
-              
-            }
+   
+ update(){
+      if (this.x != this.destX) {
+        if (this.x > this.destX) {
+          this.x -= 5;
+        }
+        if (this.x < this.destX) {
+          this.x += 5;
+        }
+      }
+      if (this.y != this.destY) {
+        if (this.y > this.destY) {
+          this.y -= 5;
+        }
+        if (this.y < this.destY) {
+          this.y += 5;
+        }
         
-          } 
-
-          if(listofshapes[i].shapetype == "bolti"){
-
-          }
       }
     }
   }
-
-
-  
-}
 
 
 
@@ -153,9 +134,33 @@ function random2(min,max) {
 
 
 
-let boltar = [];
+let hlutir = [];
 let idval = 0;
-while (boltar.length < 4) {
+let firstposX = 0;
+let firstposY = 5;
+while (firstposX < width) {
+  let size = random(100,200);
+  idval += 1;
+  let kassi = new Kassi( random(0 + size,width - size),
+  random(0 + size,height - size),
+  random(-7,7),
+  random(-7,7),
+  'rgb(' + random(0,255) + ',' + random(0,255) + ',' + random(0,255) +')',
+  size,
+  idval,
+  random2(1,500),
+  random2(25,50),
+  "kassi",
+  firstposY,
+  firstposX
+  )
+  firstposX += size + 50;
+  idval += 1;
+  hlutir.push(kassi)
+}
+
+let tel = 0;
+while (tel < 25) {
   let size = random(100,200);
   let bolti = new Bolti(
     random(0 + size,width - size),
@@ -167,35 +172,26 @@ while (boltar.length < 4) {
     idval,
     random2(1,500),
     random2(25,50),
-    "bolit"
+    "bolti"
   );
   idval += 1;
-  let kassi = new Kassi(    random(0 + size,width - size),
-  random(0 + size,height - size),
-  random(-7,7),
-  random(-7,7),
-  'rgb(' + random(0,255) + ',' + random(0,255) + ',' + random(0,255) +')',
-  size,
-  idval,
-  random2(1,500),
-  random2(25,50),
-  "kassi"
-  )
-  idval += 1;
-  /*boltar.push(bolti)*/;
-  boltar.push(kassi)
+  hlutir.push(bolti);
+  tel++
 }
-console.log(boltar);
 
 
 function loop() {
   ctx.fillStyle = "rgba(0,0,0,0.25)";
   ctx.fillRect(0,0,width, height);
 
-  for (let i = 0; i < boltar.length; i++) {
-    boltar[i].draw();
-    boltar[i].update();
-    boltar[i].coll(boltar);
+  for (let i = 0; i < hlutir.length; i++) {
+    hlutir[i].draw();
+    hlutir[i].update();   
+    if (hlutir[i].deli() == true) {
+      console.log("hello");
+    }
+    
+   
   }
 
   requestAnimationFrame(loop);
